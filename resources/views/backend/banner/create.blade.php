1f5745fb19bc617b2698d9f20d49d0e9a628db48
Kuldeep Saini <kuldeep@askmetechnologies.com>
2024-08-19 13:31:56 +0530

    replace name

--- resources/views/backend/banner/create.blade.php
+++ resources/views/backend/banner/create.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.master')
 
-@section('title','E-SHOP || Banner Create')
+@section('title','ASKME || Banner Create')
 
 @section('main-content')
 
--- resources/views/frontend/pages/product-lists.blade.php
+++ resources/views/frontend/pages/product-lists.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.layouts.master')
 
-@section('title','E-SHOP || PRODUCT PAGE')
+@section('title','ASKME || PRODUCT PAGE')
 
 @section('main-content')
 	
